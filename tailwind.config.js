e922115608839052008093c8150773bcd60a7522
Brian Marete <bmarete10@gmail.com>
2024-03-19 15:30:14 +0300

    Add product media to product page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/*.liquid',
-    './templates/customers/*.liquid',
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/*.liquid",
+    "./templates/customers/*.liquid",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        94: "22rem",
+      },
+    },
   },
   plugins: [],
-}
-
+};
